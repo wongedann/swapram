4121ad3dcaf345ed528f5df26f7648af24dd1508
Wongedan <137911586+wongedann@users.noreply.github.com>
2024-02-17 12:34:02 +0700

    Update insshws.sh

--- insshws.sh
+++ insshws.sh
@@ -6,8 +6,8 @@ apt install python -y
 
 #Install Script Websocket-SSH Python
 wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/Azigaming404/websocket/main/openssh.py && chmod +x /usr/local/bin/edu-proxy
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/messiey/rocky/master/Insshws/dropbear-ws.py.txt
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/messiey/rocky/master/Insshws/ws-stunnel.txt
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/wongedann/swapram/master/dropbear-ws.py.txt
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/wongedann/swapram/master/ws-stunnel.txt
 #wget -O /usr/local/bin/edu-proxyovpn https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/ovpn.py && chmod +x /usr/local/bin/edu-proxyovpn
 
 #izin permision
@@ -22,10 +22,10 @@ chmod +x /usr/local/bin/edu-proxyovpn
 wget -O /etc/systemd/system/edu-proxy.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/http.service && chmod +x /etc/systemd/system/edu-proxy.service
 
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/messiey/rocky/master/Insshws/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/wongedann/swapram/master/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/messiey/rocky/master/Insshws/ws-stunnel.service.txt && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/wongedan/swapram/master/ws-stunnel.service.txt && chmod +x /etc/systemd/system/ws-stunnel.service
 
 ##System Websocket-OpenVPN Python
 #wget -O /etc/systemd/system/edu-proxyovpn.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/ovpn.service && chmod +x /etc/systemd/system/edu-proxyovpn.service
